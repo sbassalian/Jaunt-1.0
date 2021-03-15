603ce636521e1cd4d358851b8113416a706d6409
sbassalian <solomonbassalian@gmail.com>
2021-03-15 18:35:32 -0400

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,5 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :destroy, :show]
   end
 end
+
+
--- frontend/jaunt.jsx
+++ frontend/jaunt.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const store = configureStore()
+    // const store = configureStore()
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Jaunt! store={store}</h1>, root);
+    ReactDOM.render(<h1>Jaunt!</h1>, root);
 });
 
