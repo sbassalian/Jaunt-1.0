f5fc46973df65665a9a6a273ce0a5d97fbfb4eaa
sbassalian <solomonbassalian@gmail.com>
2021-03-19 09:53:12 -0400

    fixed user auth a little

--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -3,17 +3,14 @@ import { logout } from '../../actions/session';
 import NavBar from './navbar';
 import {openModal} from '../../actions/modal'
 
-// const mapStateToProps = state => {
-//     console.log(state);
+const mapStateToProps = state => {
+    console.log(state);
 
-//     return {
-//         currentUser: state.entities.users[state.session.id]
-//     };
-// };
+    return {
+        currentUser: state.entities.users[state.session.id]
+    };
+};
 
-const mapStateToProps = ({ session }) => ({
-    currentUser: session.currentUser
-});
 
 
 const mapDispatchToProps = dispatch => ({
