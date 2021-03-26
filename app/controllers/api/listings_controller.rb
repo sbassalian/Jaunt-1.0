90ceba91044d56668f94d992975c2daefb6b9caf
sbassalian <solomonbassalian@gmail.com>
2021-03-26 10:04:28 -0400

    pres

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -2,8 +2,7 @@ class Api::ListingsController < ApplicationController
   before_action :require_logged_in, only: [:create]
 
   def index
-    # listings = bounds ? Listing.in_bounds(bounds) : Listing.all
-    # @listings = listings.where(num_guests_avail: guests_allowed)
+   
     @listings = Listing.all
     
     render :index
