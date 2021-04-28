9c4f0b71d284b52870ee1a6f64eb580c7739a5ff
sbassalian <solomonbassalian@gmail.com>
2021-04-28 16:34:30 -0400

    bug fix2

--- frontend/reducers/listings_reducer.js
+++ frontend/reducers/listings_reducer.js
@@ -10,7 +10,7 @@ const listingsReducer = (initialState = {}, action) => {
             return Object.assign({}, { [action.listing.id]: action.listing });
         case RECEIVE_REVIEW:
             const { review, average_rating } = action;
-            const newState = Object.assign({}, state);
+            const newState = Object.assign({}, initialState);
             newState[review.listing_id].reviewIds.push(review.id);
             newState[review.listing_id].average_rating = average_rating;
             return newState;
